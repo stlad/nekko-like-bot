32c80b36bd2275cd41bf8312f3455f84aa53e225
Vaganov Vladislav <vaganovlad@gmail.com>
2025-05-09 16:14:06 +0500

    ProcessEngine

--- nekkolike/src/main/java/ru/vaganov/nekkolike/bot/commands/MessageCommandMapper.java
+++ nekkolike/src/main/java/ru/vaganov/nekkolike/bot/commands/MessageCommandMapper.java
@@ -20,40 +20,6 @@ public class MessageCommandMapper {
         return BotCommand.NONE;
     }
 
-
-//
-//
-//
-//    public BotCommand extractCommand(Update update) {
-//        if (update.getMessage() != null && update.getMessage().hasPhoto()) {
-//            return BotCommand.SAVE_PHOTO;
-//        } else if (update.getCallbackQuery() != null) {
-//            return BotCommand.fromString(getParamFromCallback(update));
-//        } else if (update.getMessage() != null) {
-//            if (update.getMessage().getText() != null && !update.getMessage().getText().startsWith("/")) {
-//                var chatId = TelegramBotUtils.extractChatId(update);
-//                return getCommandByCurrentProcess(chatId);
-//            } else {
-//                return BotCommand.fromString(getParamFromMessage(update));
-//            }
-//        }
-//        return BotCommand.NONE;
-//    }
-//
-//    private BotCommand getCommandByCurrentProcess(Long chatId) {
-//        var process = processContext.getProcess(chatId);
-//        if (processContext.isInState(process, StartState.class)) {
-//            return BotCommand.START;
-//        }
-//        if (processContext.isInState(process, WaitForUsernameState.class)) {
-//            return BotCommand.USERNAME_RECEIVED;
-//        }
-//        if (processContext.isInState(process, MainMenuState.class)) {
-//            return BotCommand.MOVE_TO_MAIN_MENU;
-//        }
-//        return BotCommand.NONE;
-//    }
-
     private String getParamFromCallback(Update update) {
         return update.getCallbackQuery().getData();
     }
